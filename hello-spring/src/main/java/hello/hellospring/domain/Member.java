f715695f63635fae4368365d2cf3791e77405a6b
minsu <bill7666@naver.com>
2022-06-27 15:17:54 +0900

    0627

---
+++ hello-spring/src/main/java/hello/hellospring/domain/Member.java
@@ -0,0 +1,22 @@
+package hello.hellospring.domain;
+
+public class Member {
+    private Long Id;
+    private String name;
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
